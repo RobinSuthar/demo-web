0d18ba78b877070fdc2602ebb3fbc2c159b85b01
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-10-01 20:01:14 -0600

    Added High Level dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+import data from "@/app/dashboard/data.json";
 
 export default function Page() {
   return (
@@ -11,7 +16,17 @@ export default function Page() {
           <AppSidebar />
           <SidebarInset>
             <div className="flex flex-1 flex-col gap-4 p-4">
-              <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+              <div className="flex flex-1 flex-col">
+                <div className="@container/main flex flex-1 flex-col gap-2">
+                  <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                    <SectionCards />
+                    <div className="px-4 lg:px-6">
+                      <ChartAreaInteractive />
+                    </div>
+                    <DataTable data={data} />
+                  </div>
+                </div>
+              </div>
             </div>
           </SidebarInset>
         </div>
